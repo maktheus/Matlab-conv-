a582a376d1e96b7301f65e417d93f5a768c5632d
Matheus Serrao Uchoa <maktheus@gmail.com>
2023-02-08 23:37:18 -0400

    Read.me

---
+++ main.m
@@ -0,0 +1,12 @@
+b = [0.1, 0.2, 0.3];
+x = [1, 0, 0, 2, 0, 1, 4, 3];
+
+n = length(b);
+m = length(x);
+y = zeros(1, n + m - 1);
+
+for i = 1:n + m - 1
+    for j = max(1, i - m + 1):min(i, n)
+        y(i) = y(i) + b(j) * x(i - j + 1);
+    end
+end
